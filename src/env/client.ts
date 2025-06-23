c0cb9012ded79bc6ace6906852b74ab7f4fe2c95
Jaden Bertino <jaden@bertinofamily.com>
2025-06-23 08:47:41 -0700

    move VERSION to server env

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,18 +1,14 @@
-import fs from 'fs'
-import path from 'path'
 import { z } from 'zod'
 
 function validateClientEnv() {
   const clientSchema = z.object({
     ENVIRONMENT: z.enum(['development', 'staging', 'production']),
-    VERSION: z.string().default(getVersionFromChangelog()),
   })
 
   // Need to set process.env.KEYNAME because next.js inlines at build time
   type ClientEnvKeys = keyof z.infer<typeof clientSchema>
   const rawClientEnv: Record<ClientEnvKeys, string | undefined> = {
     ENVIRONMENT: process.env['NEXT_PUBLIC_ENVIRONMENT'],
-    VERSION: process.env['NEXT_PUBLIC_VERSION'],
   }
 
   const clientValidation = clientSchema.safeParse(rawClientEnv)
@@ -31,18 +27,6 @@ function validateClientEnv() {
   }
 
   return clientValidation.data
-}
-
-function getVersionFromChangelog(): string {
-  try {
-    const changelogPath = path.join(process.cwd(), 'CHANGELOG.md')
-    const content = fs.readFileSync(changelogPath, 'utf-8')
-    const versionRegex = /(\d+\.\d+\.\d+)/ // e.g. 1.0.0
-    const versionMatch = content.match(versionRegex)
-    return versionMatch?.[1] ?? '0.0.0'
-  } catch {
-    return '0.0.0'
-  }
 }
 
 const CLIENT_ENV = validateClientEnv()
--- src/env/server.ts
+++ src/env/server.ts
@@ -1,9 +1,11 @@
+import { fs } from '@/lib/fs'
+import path from 'path'
 import { z } from 'zod'
 import { CLIENT_ENV } from './client'
 
 function validateServerEnv() {
   const serverSchema = z.object({
-    // TODO: add stuff here
+    VERSION: z.string().default(getVersionFromChangelog()),
   })
 
   const serverValidation = serverSchema.safeParse(process.env)
@@ -17,8 +19,20 @@ function validateServerEnv() {
   }
 
   return {
-    ...serverValidation.data,
     ...CLIENT_ENV, // all client env vars are also available on the server
+    ...serverValidation.data,
+  }
+}
+
+function getVersionFromChangelog(): string {
+  try {
+    const changelogPath = path.join(process.cwd(), 'CHANGELOG.md')
+    const content = fs.readFileSync(changelogPath, 'utf-8')
+    const versionRegex = /(\d+\.\d+\.\d+)/ // e.g. 1.0.0
+    const versionMatch = content.match(versionRegex)
+    return versionMatch?.[1] ?? '0.0.0'
+  } catch {
+    return '0.0.0'
   }
 }
 
