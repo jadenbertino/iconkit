9a49cb2e14ee4f10cbee7a6c950dbf4ebde865b8
Jaden Bertino <jaden@ossa.ai>
2025-06-22 22:28:14 -0700

    update client env

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,31 +1,51 @@
+import fs from 'fs'
+import path from 'path'
 import { z } from 'zod'
 
-const clientSchema = z.object({
-  NEXT_PUBLIC_ENVIRONMENT: z.enum(['development', 'staging', 'production']),
-})
+function validateClientEnv() {
+  const clientSchema = z.object({
+    ENVIRONMENT: z.enum(['development', 'staging', 'production']),
+    VERSION: z.string().default(getVersionFromChangelog()),
+  })
 
-const getClientEnv = () => {
+  const clientValidation = clientSchema.safeParse(getRawClientEnv())
+  if (!clientValidation.success) {
+    console.error(
+      `❌ Invalid client environment variables`,
+      clientValidation.error.flatten().fieldErrors,
+    )
+    throw new Error('Invalid client environment variables')
+  }
+
+  return clientValidation.data
+}
+
+function getVersionFromChangelog(): string {
+  try {
+    const changelogPath = path.join(process.cwd(), 'CHANGELOG.md')
+    const content = fs.readFileSync(changelogPath, 'utf-8')
+    const versionRegex = /(\d+\.\d+\.\d+)/ // e.g. 1.0.0
+    const versionMatch = content.match(versionRegex)
+    return versionMatch?.[1] ?? '0.0.0'
+  } catch {
+    return '0.0.0'
+  }
+}
+
+function getRawClientEnv() {
   const clientEnv: Record<string, string | undefined> = {}
 
-  for (const key of Object.keys(clientSchema.shape)) {
+  // Process all NEXT_PUBLIC_ environment variables and remove the prefix
+  Object.keys(process.env).forEach((key) => {
     if (key.startsWith('NEXT_PUBLIC_')) {
-      clientEnv[key] = process.env[key]
+      const unprefixedKey = key.replace('NEXT_PUBLIC_', '')
+      clientEnv[unprefixedKey] = process.env[key]
     }
-  }
+  })
 
   return clientEnv
 }
 
-const clientValidation = clientSchema.safeParse(getClientEnv())
-
-if (!clientValidation.success) {
-  console.error(
-    '❌ Invalid client environment variables:',
-    clientValidation.error.flatten().fieldErrors,
-  )
-  throw new Error('Invalid client environment variables')
-}
-
-const CLIENT_ENV = clientValidation.data
+const CLIENT_ENV = validateClientEnv()
 
 export { CLIENT_ENV }
