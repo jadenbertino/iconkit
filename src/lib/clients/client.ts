fa9065bd4e97a07ad87a01bf619cbd78c9057226
Jaden Bertino <jaden@bertinofamily.com>
2025-06-26 16:56:07 -0700

    create public supabase client

--- src/lib/clients/client.ts
+++ src/lib/clients/client.ts
@@ -3,10 +3,10 @@ import { CLIENT_ENV } from '../../env/client'
 import { HttpClient } from '../http'
 import type { Database } from '../types/database.types'
 
-const supabase = createClient<Database>(
+const supabasePublic = createClient<Database>(
   CLIENT_ENV.SUPABASE_URL,
   CLIENT_ENV.SUPABASE_ANON_KEY,
 )
 const http = new HttpClient()
 
-export { http, supabase }
+export { http, supabasePublic }
