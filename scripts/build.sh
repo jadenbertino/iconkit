e53c7e41cc657a23d8073c6084ce7eb837212168
Jaden Bertino <jaden@bertinofamily.com>
2025-06-26 20:53:02 -0700

    remove doppler from build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+# If running this script locally, then do:
+# doppler run -- ./scripts/build.sh
+
 echo "🔍 Running ESLint..."
 npm run lint
 
 echo "🔄 Uploading icons to database..."
-doppler run -- npx tsx src/build/index.ts
+npx tsx src/build/index.ts
 
 echo "🏗️  Building Next.js app..."
 npx next build
