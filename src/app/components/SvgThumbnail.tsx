a49286631aeb38ec7781dd78cdb97e473a664ad2
Jaden Bertino <jaden@bertinofamily.com>
2025-07-11 12:01:51 -0700

    sanitize the svg on client side

--- src/app/components/SvgThumbnail.tsx
+++ src/app/components/SvgThumbnail.tsx
@@ -1,6 +1,7 @@
 import { centerClasses } from '@/constants/classes'
 import { cn } from '@/lib'
 import type { Icon } from '@/lib/schemas/database'
+import DOMPurify from 'dompurify'
 
 const SvgThumnail = ({
   icon,
@@ -9,6 +10,8 @@ const SvgThumnail = ({
   icon: Icon
   className?: string
 }) => {
+  const sanitizedSvg = DOMPurify.sanitize(icon.svg)
+
   return (
     <div
       className={cn(
@@ -17,7 +20,7 @@ const SvgThumnail = ({
         centerClasses,
         className,
       )}
-      dangerouslySetInnerHTML={{ __html: icon.svg }}
+      dangerouslySetInnerHTML={{ __html: sanitizedSvg }}
     />
   )
 }
