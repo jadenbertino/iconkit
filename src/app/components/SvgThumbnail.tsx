65840d788e97c9a30e36be907f5e0113dba9c3eb
Jaden Bertino <jaden@bertinofamily.com>
2025-07-11 11:59:02 -0700

    fix svg visuals

--- src/app/components/SvgThumbnail.tsx
+++ src/app/components/SvgThumbnail.tsx
@@ -1,8 +1,6 @@
 import { centerClasses } from '@/constants/classes'
-import { cn, htmlAttributesToReact } from '@/lib'
+import { cn } from '@/lib'
 import type { Icon } from '@/lib/schemas/database'
-import type { DOMNode } from 'html-react-parser'
-import parse, { domToReact } from 'html-react-parser'
 
 const SvgThumnail = ({
   icon,
@@ -11,40 +9,16 @@ const SvgThumnail = ({
   icon: Icon
   className?: string
 }) => {
-  const SvgElement = parse(icon.svg, {
-    replace: (domNode) => {
-      if (
-        domNode.type === 'tag' &&
-        (domNode.name === 'svg' ||
-          domNode.name === 'path' ||
-          domNode.name === 'circle' ||
-          domNode.name === 'rect')
-      ) {
-        return (
-          <domNode.name
-            {...htmlAttributesToReact(domNode.attribs)}
-            fill={domNode.attribs['fill'] ?? 'currentColor'}
-            stroke={domNode.attribs['stroke'] ?? 'currentColor'}
-            className='w-full h-full'
-          >
-            {domToReact(domNode.children as DOMNode[])}
-          </domNode.name>
-        )
-      }
-      return null
-    },
-  })
-
   return (
     <div
       className={cn(
-        'w-16 h-16 p-2 bg-white rounded-lg shadow-md text-black',
+        'w-16 h-16 p-2 bg-white rounded-lg shadow-md',
+        'text-black [&>svg]:w-full [&>svg]:h-full [&>svg]:object-contain',
         centerClasses,
         className,
       )}
-    >
-      {SvgElement}
-    </div>
+      dangerouslySetInnerHTML={{ __html: icon.svg }}
+    />
   )
 }
 
