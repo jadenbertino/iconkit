ed45dfeedcad9bdec01bfeeb5a6f8dd3251fc907
Jaden Bertino <jaden@bertinofamily.com>
2025-07-11 11:51:56 -0700

    use shadcn button

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { useSearch } from '../context/SearchContext'
 
 export function Pagination() {
@@ -5,23 +6,16 @@ export function Pagination() {
 
   return (
     <div className='relative flex items-center justify-between w-full mt-6'>
-      {search.page > 1 && (
-        <button
-          onClick={prevPage}
-          className='px-4 py-2 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200'
-        >
-          Previous
-        </button>
-      )}
-      <span className='absolute left-1/2 transform -translate-x-1/2 px-4 py-2 text-gray-600'>
+      {search.page > 1 && <Button onClick={prevPage}>Previous</Button>}
+      <span className='absolute left-1/2 transform -translate-x-1/2 py-2 px-4 text-gray-600'>
         Page {search.page}
       </span>
-      <button
+      <Button
         onClick={nextPage}
-        className='ml-auto px-4 py-2 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200'
+        className='ml-auto'
       >
         Next
-      </button>
+      </Button>
     </div>
   )
 }
--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { useState } from 'react'
 import { useSearch } from '../context/SearchContext'
 
@@ -25,12 +26,12 @@ export function SearchBar() {
         onKeyDown={handleKeyPress}
         className='flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
       />
-      <button
+      <Button
         onClick={handleSearch}
-        className='px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+        className='bg-blue-500 hover:bg-blue-500/90'
       >
         Search
-      </button>
+      </Button>
     </div>
   )
 }
