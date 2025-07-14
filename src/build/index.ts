dfa4f5f8fc8d1f18d48e2185bd8ccb7cfb043d48
Jaden Bertino <jaden@bertinofamily.com>
2025-07-14 15:05:34 -0700

    upload licenses on build

--- src/build/index.ts
+++ src/build/index.ts
@@ -2,20 +2,25 @@ import { ICON_PROVIDER_SLUGS } from '@/constants'
 import { serverLogger } from '@/lib/logs/server'
 import { scrapeIcons } from './icons/scrape'
 import { uploadIcons } from './icons/upload'
+import { scrapeLicenses } from './licenses/scrape'
+import { uploadLicenses } from './licenses/upload'
 
 async function uploadAssets(): Promise<void> {
   // Upload icons
-  let count = 0
+  let iconCount = 0
   await Promise.all(
     ICON_PROVIDER_SLUGS.map(async (provider) => {
       const icons = await scrapeIcons(provider)
-      count += icons.length
+      iconCount += icons.length
       await uploadIcons(icons, provider)
     }),
   )
-  serverLogger.info(`⭐ Uploaded ${count} icons`)
+  serverLogger.info(`⭐ Uploaded ${iconCount} icons`)
 
   // Upload licenses
+  const scrapedLicenses = await scrapeLicenses()
+  const uploadedLicenses = await uploadLicenses(scrapedLicenses)
+  serverLogger.info(`📄 Uploaded ${uploadedLicenses.length} licenses`)
 
   process.exit(0) // file continues to run for some reason so we need to exit
 }
