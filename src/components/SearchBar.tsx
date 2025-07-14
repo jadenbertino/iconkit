d8b00125bd5bc81aa96f47d20401c854dfb76297
Jaden Bertino <jaden@bertinofamily.com>
2025-07-13 20:19:54 -0700

    add search icon to search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useDebounceCallback } from 'usehooks-ts'
 import { useSearch } from '../context/SearchContext'
+import { MagnifyingGlassHero } from './icons/MagnifyingGlass'
 
 export function SearchBar() {
   const { setSearch, search } = useSearch()
@@ -18,13 +19,16 @@ export function SearchBar() {
   }
 
   return (
-    <div className='flex items-center gap-2'>
+    <div className='flex items-center gap-2 relative'>
+      <div className='absolute left-0 top-1/2 -translate-y-1/2 pointer-events-none w-10 h-full flex items-center justify-center'>
+        <MagnifyingGlassHero className='size-6 text-gray-500' />
+      </div>
       <input
         type='text'
         placeholder='Search icons...'
         value={searchText}
         onChange={handleInputChange}
-        className='flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
+        className='flex-1 pr-4 pl-10 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-500 focus:border-transparent'
       />
     </div>
   )
---
+++ src/components/icons/MagnifyingGlass.tsx
@@ -0,0 +1,42 @@
+const MagnifyingGlass16 = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='16'
+    height='16'
+    viewBox='0 0 16 16'
+  >
+    <path d='M10.68 11.74a6 6 0 0 1-7.922-8.982 6 6 0 0 1 8.982 7.922l3.04 3.04a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215ZM11.5 7a4.499 4.499 0 1 0-8.997 0A4.499 4.499 0 0 0 11.5 7Z' />
+  </svg>
+)
+
+const MagnifyingGlassFA = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 512 512'
+  >
+    <path d='M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z' />
+  </svg>
+)
+
+const MagnifyingGlassHero = ({
+  className,
+}: {
+  className?: string
+}) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 16 16'
+    fill='currentColor'
+    aria-hidden='true'
+    data-slot='icon'
+    className={className}
+  >
+    <path
+      fillRule='evenodd'
+      d='M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z'
+      clipRule='evenodd'
+    />
+  </svg>
+)
+
+export { MagnifyingGlass16, MagnifyingGlassFA, MagnifyingGlassHero }
