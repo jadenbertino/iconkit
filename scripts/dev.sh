5c4575359ce3e3e529a292460b72ba0d4b1e9c23
Jaden Bertino <jaden@bertinofamily.com>
2025-07-29 12:08:45 -0700

    never use turbopack

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -5,9 +5,5 @@ export ENVIRONMENT=${ENVIRONMENT:-development}
 source scripts/utils/get_doppler_token.sh
 export DOPPLER_TOKEN
 
-# Was having issues with turbopack on mac
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  doppler run -- npx next dev
-else
-  doppler run -- npx next dev --turbopack
-fi
+# Was having issues with turbopack
+doppler run -- npx next dev 
