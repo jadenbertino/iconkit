7a8fec5c62da19d2ba63d84f40c775525d5ee499
Jaden Bertino <jaden@bertinofamily.com>
2025-07-31 16:17:29 -0700

    all pages display navbar + footer

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,3 @@
-import Footer from '@/components/Footer'
-import { Navbar } from '@/components/Navbar'
 import { BenefitsSection } from './BenefitsSection'
 import { HeroSection } from './HeroSection'
 import { LibraryCarousel } from './LibraryCarousel'
@@ -8,16 +6,12 @@ import { ThreeCardSection } from './ThreeCardSection'
 
 export default function HomePage() {
   return (
-    <div className='min-h-screen bg-canvas'>
-      <Navbar />
-      <div className='space-y-0'>
-        <HeroSection />
-        <LibraryCarousel />
-        <ThreeCardSection />
-        <BenefitsSection />
-        <PricingSection />
-        <Footer />
-      </div>
-    </div>
+    <>
+      <HeroSection />
+      <LibraryCarousel />
+      <ThreeCardSection />
+      <BenefitsSection />
+      <PricingSection />
+    </>
   )
 }
